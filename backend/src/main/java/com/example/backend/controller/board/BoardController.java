b4c46eb5cd0d39f925256e3a762a980fb3253611
Jin19121 <jinkim19121@gmail.com>
2024-11-14 11:21:29 +0900

    backend : add 받기 mapper & service

--- backend/src/main/java/com/example/backend/controller/board/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/board/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.backend.controller.board;
 
 import com.example.backend.dto.board.Board;
+import com.example.backend.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
+
+  final BoardService service;
+
   @PostMapping("add")
   public void add(@RequestBody Board board) {
-    System.out.println(board);
-
+    service.add(board);
   }
 }
