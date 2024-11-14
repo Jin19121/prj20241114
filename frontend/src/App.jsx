57c2eb4a3e07d09d7b95ed900bb7e118eac77909
Jin19121 <jinkim19121@gmail.com>
2024-11-14 12:38:07 +0900

    view {id} 컴포 작성중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { BoardAdd } from "./page/board/BoardAdd.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
 import { RootLayout } from "./page/root/RootLayout.jsx";
+import { BoardView } from "./components/board/BoardView.jsx";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <BoardList /> },
       { path: "add", element: <BoardAdd /> },
+      { path: "view/:id", element: <BoardView /> },
     ],
   },
 ]);
