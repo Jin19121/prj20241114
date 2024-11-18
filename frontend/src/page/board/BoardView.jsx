af8e6be311c2cd2fc7aebf1cc3d3fd9d2c1ebd71
Jin19121 <jinkim19121@gmail.com>
2024-11-18 09:10:41 +0900

    게시글 삭제 toaster

--- frontend/src/page/board/BoardView.jsx
+++ frontend/src/page/board/BoardView.jsx
@@ -1,16 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Box, Input, Spinner, Stack, Textarea } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Field } from "../../components/ui/field.jsx";
 import { Button } from "../../components/ui/button.jsx";
+import { toaster } from "../../components/ui/toaster.jsx";
 
 export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get(`api/boar/view/${id}`).then((res) => setBoard(res.data));
+    axios.get(`/api/board/view/${id}`).then((res) => setBoard(res.data));
   }, []);
 
   if (board === null) {
@@ -18,25 +20,42 @@ export function BoardView() {
   }
 
   const handleDeleteClick = () => {
-    axios.delete(`/api/board/delete/${board.id}`);
+    axios
+      .delete(`/api/board/delete/${board.id}`)
+      .then((res) => res.data)
+      .then((data) => {
+        toaster.create({
+          type: data.message.type,
+          description: data.message.text,
+        });
+        navigate("/");
+      })
+      .catch((e) => {
+        const data = e.response.data;
+        toaster.create({
+          type: data.message.type,
+          description: data.message.text,
+        });
+      });
   };
 
   return (
     <Box>
-      <h3>{id}번 게시물</h3>
+      <h3>{id} 번 게시물</h3>
       <Stack gap={5}>
-        <Field label={"제목"} readOnly>
+        <Field label="제목" readOnly>
           <Input value={board.title} />
         </Field>
-        <Field label={"본문"} readOnly>
+        <Field label="본문" readOnly>
           <Textarea value={board.content} />
         </Field>
-        <Field label={"작성자"} readOnly>
+        <Field label="작성자" readOnly>
           <Input value={board.writer} />
         </Field>
-        <Field label={"작성일시"} readOnly>
+        <Field label="작성일시" readOnly>
           <Input value={board.inserted} type={"datetime-local"} />
         </Field>
+
         <Box>
           <Button colorPalette={"red"} onClick={handleDeleteClick}>
             삭제
