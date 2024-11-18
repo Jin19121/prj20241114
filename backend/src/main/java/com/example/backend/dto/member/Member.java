69f9432d4c78fb484a553f9c54f0c90fcac50924
Jin19121 <jinkim19121@gmail.com>
2024-11-18 09:38:29 +0900

    Member dto 설정

---
+++ backend/src/main/java/com/example/backend/dto/member/Member.java
@@ -0,0 +1,13 @@
+package com.example.backend.dto.member;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Member {
+  private String id;
+  private String password;
+  private String description;
+  private LocalDate inserted;
+}
