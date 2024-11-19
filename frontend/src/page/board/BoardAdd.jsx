319ee721c774e4f73c5b5c91f610b7a18f62aa68
Jin19121 <jinkim19121@gmail.com>
2024-11-19 12:18:21 +0900

    로그인된 사용자 = 글 작성자

--- frontend/src/page/board/BoardAdd.jsx
+++ frontend/src/page/board/BoardAdd.jsx
@@ -9,7 +9,6 @@ import { toaster } from "../../components/ui/toaster.jsx";
 export function BoardAdd() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [writer, setWriter] = useState("");
   const navigate = useNavigate();
   const [progress, setProgress] = useState(false);
 
@@ -17,11 +16,18 @@ export function BoardAdd() {
     setProgress(true);
 
     axios
-      .post("/api/board/add", {
-        title,
-        content,
-        writer, //이름이 같으면 생략 가능 title : title
-      })
+      .post(
+        "/api/board/add",
+        {
+          title, //이름이 같으면 생략 가능 title : title
+          content,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        },
+      )
       .then((res) => res.data)
       .then((data) => {
         const message = data.message;
@@ -59,9 +65,6 @@ export function BoardAdd() {
             value={content}
             onChange={(e) => setContent(e.target.value)}
           />
-        </Field>
-        <Field label={"작성자"}>
-          <Input value={writer} onChange={(e) => setWriter(e.target.value)} />
         </Field>
         <Box>
           <Button
