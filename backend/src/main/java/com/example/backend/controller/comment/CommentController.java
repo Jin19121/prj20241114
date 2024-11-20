947e9e5eceb0a2defabf57b2effc0fba5521d688
Jin19121 <jinkim19121@gmail.com>
2024-11-20 10:44:10 +0900

    댓글 : 테이블 생성, 댓글 작성

---
+++ backend/src/main/java/com/example/backend/controller/comment/CommentController.java
@@ -0,0 +1,26 @@
+package com.example.backend.controller.comment;
+
+import com.example.backend.dto.comment.Comment;
+import com.example.backend.service.comment.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/comment")
+public class CommentController {
+  final CommentService service;
+
+  @PostMapping
+  @PreAuthorize("isAuthenticated()")
+  public void add(@RequestBody Comment comment, Authentication auth) {
+    System.out.println("comment = " + comment);
+    service.add(comment, auth);
+  }
+
+}
---
+++ frontend/src/components/comment/CommentContainer.jsx
@@ -0,0 +1,15 @@
+import { Box, Stack } from "@chakra-ui/react";
+import { CommentList } from "./CommentList.jsx";
+import { CommentInput } from "./CommentInput.jsx";
+
+export function CommentContainer({ boardId }) {
+  return (
+    <Box>
+      <Stack gap={5}>
+        <h3>댓글</h3>
+        <CommentInput boadId={boardId} />
+        <CommentList />
+      </Stack>
+    </Box>
+  );
+}
